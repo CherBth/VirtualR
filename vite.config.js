3fe13909c968bcb9cdf0e50f7cbca8b82a51c98d
Cherifa Bouthour <bouthourchrifa2@gmail.com>
2025-04-05 00:01:14 +0200

    Fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : process.env.VITE_BASE_PATH || "/VirtualR",
-})
+  base: "./",  // Au lieu de "/VirtualR"
+  build: {
+    outDir: "dist",
+  },
+});
+
